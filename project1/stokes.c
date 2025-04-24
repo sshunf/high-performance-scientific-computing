1e7cb7f97b8004493d8db67b564a5cce050b7496
Shun Fujita <iiy0546@lagrange.esam.northwestern.edu>
2025-04-24 13:04:59 -0500

    binaries

--- project1/stokes.c
+++ project1/stokes.c
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
 
 	// apply boundary conditions TODO: check if we need this
 	for (int k = 0; k < N-1; ++k){
-		p[0  ][k] = P;
+		p[0][k] = P;
 		p[N-2][k] = 0.0;
 	}
 
@@ -50,16 +50,19 @@ int main(int argc, char* argv[]) {
 	for (int i = 0; i < K; ++i) {
 		
 		// udpate the u values
+
+		// update bottom left
 		u_res = mu * (
             -4*u[0][0]
             + u[1][0]
             + u[0][1]
           )
-        - 2*dy*(p[0][0] - P);
+        - 2*dy*(p[0][0] - P); // TODO: check this part
 
-		u[0][0] += omega*u_res; // update bottom left
+		u[0][0] += omega*u_res;
 		max_residual = max(u_res, max_residual);
 
+		// update left side (inlet)
 		for(int k = 1; k < N-2; ++k) {
 			u_res = mu * (
 						-3*u[0][k]
@@ -68,7 +71,7 @@ int main(int argc, char* argv[]) {
 						+ u[0][k+1]
 					)
 					- 2*dy*(p[0][k] - P);
-			u[0][k] += omega * u_res; // update left side (inlet)
+			u[0][k] += omega * u_res; 
 			max_residual = max(u_res, max_residual);
 		} 
 
@@ -80,6 +83,7 @@ int main(int argc, char* argv[]) {
 		  )
 		- 2*dy*(p[1][N-2] - p[0][N-2]);
 		
+		 // update bottom row
 		for (int j = 1; j < N-1; ++j) {
 			u_res = mu * (
 				-5*u[j][0]
@@ -88,43 +92,47 @@ int main(int argc, char* argv[]) {
 				+ u[j+1][0]
 			  )
 			- dy * (p[j][0] - p[j-1][0]);
-			u[j][0] += omega * u_res; // update bottom row
+			u[j][0] += omega * u_res; 
 			max_residual = max(u_res, max_residual);
 			
+			 // update interior
 			for (int k = 1; k < N-2; ++k) {
 				u_res = mu * (
-					u[j-1][k]
 					- 4*u[j][k]
+					+ u[j-1][k]
 					+ u[j+1][k]
 					+ u[j][k-1]
 					+ u[j][k+1]
 				) 
 				- dy * (p[j][k] - p[j-1][k]);
 
-				u[j][k] += omega*u_res; // update interior
+				u[j][k] += omega*u_res;
 				max_residual = max(u_res, max_residual);	
 			}
 
+			// update top row
 			u_res = mu * (
 				-5*u[j][N-2]
 				+ u[j-1][N-2]
 				+ u[j][N-3]
 				+ u[j+1][N-2]
 			  )
-			- dy * (p[j][0] - p[j-1][0]);
-			u[j][N-2] += omega * u_res; // update bottom row
+			- dy * (p[j][N-2] - p[j-1][N-2]);
+			u[j][N-2] += omega * u_res; 
 			max_residual = max(u_res, max_residual);	
 		}
 
+		// update bottom right
 		u_res = mu * (
 			-4*u[N-1][0]
 			+ u[N-2][0]
 			+ u[N-1][1]
 		  )
-		- dy * (p[N-2][0] - p[N-3][0]);
-		u[N-1][0] += omega * u_res; // update bottom right
+		- dy * (p[N-1][0] - p[N-2][0]);
+		u[N-1][0] += omega * u_res;
 		max_residual = max(u_res, max_residual);	
 
+		 // update right side (outlet)
 		for (int k = 1; k < N-2; ++k) {
 			u_res = mu * (
 				-3*u[N-1][k]
@@ -132,60 +140,101 @@ int main(int argc, char* argv[]) {
 				+ u[N-1][k-1]
 				+ u[N-1][k+1]
 			)
-			- dy * (p[N-2][k] - p[N-3][k]);
-			u[N-1][k] += omega * u_res; // update right side (outlet)
+			- dy * (p[N-1][k] - p[N-2][k]);
+			u[N-1][k] += omega * u_res;
 			max_residual = max(u_res, max_residual);	
 		}
 
+		// update top right
 		u_res = mu * (
 			-4*u[N-1][N-2]
 			+ u[N-2][N-2]
 			+ u[N-1][N-3]
 		  )
-		- dy * (p[N-2][N-2] - p[N-3][N-2]);
-		u[N-1][N-2] += omega * u_res; // update top right
-		max_residual = max(u_res, max_residual);		
+		- dy * (p[N-1][N-2] - p[N-2][N-2]);
+		u[N-1][N-2] += omega * u_res;
+		max_residual = max(u_res, max_residual);
 
 
 		// update the v values
 
-		// update bottom left
+		// update bottom left TODO: check if these are right
+		// v_res = mu * (
+		// 	-3*v[0][0]
+		// 	+ v[1][0]
+		// 	+ v[0][1]
+		// )
+		// - 2 * dx * (p[0][0] - P);
+
+		// v[0][0] += omega*v_res;
+		// max_residual = max(u_res, max_residual);
 		v[0][0] = 0;
 		
 		// update left side
 		for (int k = 1; k < N-1; ++k) {
 			v_res = mu * (
-				3*v[0][k]
+				-3*v[0][k]
 				+ v[1][k]
 				+ v[0][k+1]
 				+ v[0][k-1]
 			)
 			- dx * (p[0][k] - p[0][k-1]);
 			v[0][k] += omega*v_res;
+			max_residual = max(v_res, max_residual);
 		}
 
 		// update top left
-		v[0][N-1] = 0;
+		// v_res = mu * (
+		// 	-3*v[0][N-1]
+		// 	+ v[1][N-1]
+		// 	+ v[0][N-2]
+		// )
+		// - dx * 2 * (p[0][N-1] - P);
+
+		// v[0][N-2] += omega*v_res;
+		// max_residual = max(v_res, max_residual);
+		v[0][N-2] = 0;
 
 		for (int j = 1; j < N-2; ++j) {
 			// update bottom
+			// v_res = mu * (
+			// 	-3*v[j][0]
+			// 	+ v[j-1][0]
+			// 	+ v[j+1][0]
+			// 	+ v[j][1]
+			// )
+			// - dx * 2 * (p[j][0] - P);
+
+			// v[j][0] += omega*v_res;
+			// max_residual = max(v_res, max_residual);	
 			v[j][0] = 0;
 
+			// update interior
 			for (int k = 1; k < N-1; ++k) {
 				v_res = mu * (
-					v[j-1][k] 
 					- 4*v[j][k] 
+					+ v[j-1][k] 
 					+ v[j+1][k]
 					+ v[j][k+1] 
 					+ v[j][k-1]
 				)
 				- dx*(p[j][k] - p[j][k-1]);
 
-				v[j][k] += omega*v_res; // update interior
+				v[j][k] += omega*v_res; 
 				max_residual = max(v_res, max_residual);
 			}
 
 			// update top
+			// v_res = mu * (
+			// 	-3*v[j][N-1]
+			// 	+ v[j-1][N-1]
+			// 	+ v[j+1][N-1]
+			// 	+ v[j][N-1]
+			// )
+			// - dx * (v[j][N-1] - v[j][N-2]);
+
+			// v[j][N-1] += omega*v_res;
+			// max_residual = max(v_res, max_residual);
 			v[j][N-1] = 0;
 		}
 
@@ -218,7 +267,7 @@ int main(int argc, char* argv[]) {
 			v[k][0]   = v[k][1];
 			v[k][N-1] = v[k][N-2];
 		}
-		
+
 		// update the p values
 
 		// update bottom left
@@ -289,19 +338,47 @@ int main(int argc, char* argv[]) {
 		p[N-2][N-2] += omega*p_res;
 		max_residual = max(p_res, max_residual);	
 
+		// TODO: Check if we need to apply BCs here
+		for (int j = 0; j < N; ++j) {
+			u[j][0] = 0.0;
+			u[j][N-2] = 0.0;
+		}
+
+		for (int j = 0; j < N-1; ++j) {
+			v[j][0] = 0.0;
+			v[j][N-1] = 0.0;
+		}
+
+		for(int j = 0; j < N; ++j){
+			v[0][j] = 0.0;
+			v[N-2][j] = 0.0;
+		}
+
+		for (int k = 0; k < N-1; k++) {
+			p[0][k] = P;
+			p[N-2][k] = 0;
+		}
 		
 		// break when we are within tolerance
-		if (max_residual < tol) break;
-	}
-
-
-	// delete later
-	for (int j=0; j<N-1; ++j){
-		for (int k=0; k<N; ++k){
-			printf("%.f ", u[j][k]);
+		if (max_residual < tol) {
+			printf("number of iterations: %d\n", i);
+			break;
 		}
-		printf("\n");
 	}
+
+
+	// write data to files
+	FILE *fU = fopen("StokesU.out", "wb");
+	fwrite(u, sizeof(double), N*(N-1), fU);
+	fclose(fU);
+
+	FILE *fV = fopen("StokesV.out","wb");
+	fwrite(v, sizeof(double), (N-1)*N, fV);
+	fclose(fV);
+
+	FILE *fP = fopen("StokesP.out","wb");
+	fwrite(p, sizeof(double), (N-1)*(N-1), fP);
+	fclose(fP);
 
 	free(u);
 	free(v);
